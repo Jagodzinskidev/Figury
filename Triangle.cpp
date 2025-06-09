a82a8b26b0dcff51f4de6e09df169da8f4b9ec72
student <student@p.lodz.pl>
2025-06-09 13:52:31 +0200

    Added triangle

--- Triangle.cpp
+++ Triangle.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Triangle.h"
+#include <math.h>
 
 Triangle::Triangle(string name, float a) : Figure(name)
 {
@@ -11,18 +12,18 @@ Triangle::Triangle(string name, float a) : Figure(name)
 
 float Triangle::Area() const
 {
-    return m_area;
+    return ((m_a*m_a*sqrt(3))/4);
 }
 
 float Triangle::Perimeter() const
 {
-    return m_perimeter;
+    return (3*m_a);
 }
 
 void Triangle::Info() const
 {
     cout << "Rectangle: " << Name() << endl;
     cout << "Sides length: " << m_a << endl;
-    cout << "Area: " << m_area << endl;
-    cout << "Perimeter: " << m_perimeter << endl;
+    cout << "Area: " << ((m_a*m_a*sqrt(3))/4) << endl;
+    cout << "Perimeter: " << (3*m_a) << endl;
 }
